d09c8bf46d6899ddb877f7f8eb960655afd2f011
xairaven <alexkovalevkpi@gmail.com>
2024-06-30 03:54:57 +0300

    Feat: Custom Error module

---
+++ media_fetch_bot/src/error.rs
@@ -0,0 +1,10 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum BotError {
+    #[error("Something wrong with .env file. May be, it not exists.")]
+    EnvIsNotLoaded,
+
+    #[error("Something wrong with .env BOT_TOKEN. May be, there's not field with this name.")]
+    EnvBotToken,
+}
