7a4b2926a5bc27ee5bef3b483c5cc1f9f8a48c56
xairaven <alexkovalevkpi@gmail.com>
2024-06-30 04:59:33 +0300

    Feat: Handling command + standard messages

--- media_fetch_bot/src/main.rs
+++ media_fetch_bot/src/main.rs
@@ -2,6 +2,8 @@ use std::process;
 use teloxide::{prelude::*, utils::command::BotCommands};
 use media_fetch_bot::config::Config;
 
+static BOT_NAME: &str = "sm_media_fetch_bot";
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
@@ -16,7 +18,25 @@ async fn main() {
 
     let bot = Bot::new(&config.bot_token);
 
-    Command::repl(bot, answer).await;
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        handle_message(bot, msg).await;
+        respond(())
+    })
+        .await;
+}
+
+async fn handle_message(bot: Bot, msg: Message) {
+    let text = msg.text().unwrap_or("");
+
+    // Check if the message is a command
+    if let Ok(command) = Command::parse(text, &BOT_NAME) {
+        handle_command(bot, msg, command).await
+            .log_on_error().await;
+    } else {
+        // Handle non-command messages
+        bot.send_message(msg.chat.id, "Command not found").await
+            .log_on_error().await;
+    }
 }
 
 #[derive(BotCommands, Clone)]
@@ -26,7 +46,7 @@ enum Command {
     Help,
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+async fn handle_command(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
         Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
     };
