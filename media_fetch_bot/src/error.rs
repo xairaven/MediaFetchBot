051f29bee410f937f68cfda49d39cc0696c549cc
xairaven <alexkovalevkpi@gmail.com>
2024-06-30 09:44:16 +0300

    Fix: Spelling mistake in error descriptions

--- media_fetch_bot/src/error.rs
+++ media_fetch_bot/src/error.rs
@@ -5,9 +5,9 @@ pub enum BotError {
     #[error("Something wrong with .env file. May be, it not exists.")]
     EnvIsNotLoaded,
 
-    #[error("Something wrong with .env BOT_TOKEN. May be, there's not field with this name.")]
+    #[error("Something wrong with .env BOT_TOKEN. May be, there's no field with this name.")]
     EnvBotToken,
 
-    #[error("Something wrong with .env BOT_NAME. May be, there's not field with this name.")]
+    #[error("Something wrong with .env BOT_NAME. May be, there's no field with this name.")]
     EnvBotName,
 }
