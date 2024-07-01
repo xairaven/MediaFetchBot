63e41d969a1192e94df69ef19689fb1aac4534c4
xairaven <alexkovalevkpi@gmail.com>
2024-07-01 09:22:22 +0300

    Feat: New localization entry, LinkTypeUndefined

--- media_fetch_bot/src/localization.rs
+++ media_fetch_bot/src/localization.rs
@@ -2,9 +2,15 @@ use strum_macros::{Display, EnumString, IntoStaticStr};
 
 #[derive(Display, EnumString, IntoStaticStr)]
 pub enum LocalizationCommand {
+    #[strum(serialize = "command_not_found")]
+    CommandNotFound,
+
+    #[strum(serialize = "empty_message")]
+    EmptyMessage,
+
     #[strum(serialize = "help")]
     Help,
 
-    #[strum(serialize = "command_not_found")]
-    CommandNotFound
+    #[strum(serialize = "link_type_undefined")]
+    LinkTypeUndefined,
 }
