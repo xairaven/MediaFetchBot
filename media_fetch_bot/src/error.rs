08713cf749cd084a044dcbc648a0d2097bf86983
xairaven <alexkovalevkpi@gmail.com>
2024-07-01 12:22:15 +0300

    Feat: InvalidResult, NoResult, UnableToCreateFile, UnableToCopyContent errors

--- media_fetch_bot/src/error.rs
+++ media_fetch_bot/src/error.rs
@@ -17,15 +17,24 @@ pub enum BotError {
     #[error("Failed to get user message.")]
     FailedGetUserMessage,
 
-    #[error("Failed to get a response from API. Please, send that link to administrator for additional tests.")]
+    #[error("Failed to get a response from API.")]
     FailedGetResponse,
 
-    #[error("Failed to extract html. Please, send that link to administrator for additional tests.")]
+    #[error("Failed to extract html.")]
     FailedExtractingHtml,
 
-    #[error("Failed to parse response. Please, send that link to administrator for additional tests.")]
+    #[error("Failed to parse response.")]
     FailedParseResponse,
+
+    #[error("All steps done, but result is invalid.")]
+    InvalidResult,
 
     #[error("Unfortunately, there's no result by this query.")]
     NoResult,
+
+    #[error("Server problem, unable to create file for downloading.")]
+    UnableToCreateFile,
+
+    #[error("Server problem, failed to copy content.")]
+    UnableToCopyContent,
 }
