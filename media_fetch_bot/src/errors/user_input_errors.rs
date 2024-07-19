bc5befc8f8a7d306282b438eb5c2ab6031ba2f59
xairaven <alexkovalevkpi@gmail.com>
2024-07-19 14:44:32 +0300

    (Fix) Redundant CommandNotFound user input error

--- media_fetch_bot/src/errors/user_input_errors.rs
+++ media_fetch_bot/src/errors/user_input_errors.rs
@@ -2,9 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum UserInputError {
-    #[error("Command not found.")]
-    CommandNotFound,
-
     #[error("Empty message.")]
     EmptyMessage,
 
